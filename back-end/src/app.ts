30a47b73ad000671c8e4b32dc8de8e8c6a955b12
Amr-Zain <amrzain121@gmail.com>
2025-04-05 22:27:34 +0200

    refactor setup the api and add config for vercel deployment
    update vercel.json
    
    update vercel.json
    
    update book model incoding to utf-8
    
    delete book model
    
    add book model
    
     fix config

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,57 +1,30 @@
 import express from "express";
-import cors from "cors";
-import 'express-async-errors'
 import env from 'dotenv';
-import bookRouter from "./routes/book";
 import connectDB from "./db/connection";
 import notFound from "./middleware/not-found";
 import errorHandler from "./middleware/error";
-import authenticateUser from "./middleware/authentication";
-import orderRouter from "./routes/order";
-import { initializeApp, ServiceAccount } from 'firebase-admin/app';
-import { credential } from "firebase-admin";
-import adminRouter from "./routes/admin";
-import Book from "./models/book";
 
-const port = process.env.PORT || 3030; 
+import firebasCofig from "./setup/firebase";
+import routesSetup from "./setup/routes";
+import middlewaresSetup from "./setup/middlewares";
+
+const port = process.env.PORT || 3030;
 
 
 
 env.config()
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const  serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
 const app = express();
 // middleware
-app.use(express.json());
 
-app.use(cors({
-  origin: ['*','http://localhost:5173'],
-  credentials: true
-}));
-app.use(authenticateUser as unknown as express.RequestHandler);
-
-//const credentialPath = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-initializeApp({
-  credential: credential.cert(serviceAccount as ServiceAccount),
-});
-
- 
-
-// routes
-app.use('/api/v1/books',bookRouter);
-app.use('/api/v1/orders',orderRouter);
-app.use('/api/v1/admin',adminRouter);
-
-
+firebasCofig();
+middlewaresSetup(app); 
+routesSetup(app);
 
 app.use(notFound);
-app.use(errorHandler); 
-app.listen(port, async() => {
-  try{
-    await connectDB(process.env.DB_URL)
-    await Book.updateMany({},{$set:{createdAt:'2025-03-31T02:12:40.487+00:00', updatedAt:'2025-03-31T02:12:40.487+00:00'}})
-  }catch(e){
-    console.error("DB connection error: ",e)
-  }
+app.use(errorHandler);
+
+app.listen(port, async () => {
+  await connectDB(process.env.DB_URL)
   console.log(`app listening on port ${port}`);
 });
